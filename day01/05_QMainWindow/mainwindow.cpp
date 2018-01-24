3929ae59f906fa342e05c2487c9dca76a9090c05
mokusolo <liyundan3@gmail.com>
2018-01-25 00:21:08 +0800

    QStatusBar and QDockWidget

--- day01/05_QMainWindow/mainwindow.cpp
+++ day01/05_QMainWindow/mainwindow.cpp
@@ -5,6 +5,10 @@
 #include <QDebug>
 #include <QToolBar>
 #include <QPushButton>
+#include <QStatusBar>
+#include <QLabel>
+#include <QTextEdit>
+#include <QDockWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -44,8 +48,26 @@ MainWindow::MainWindow(QWidget *parent)
             );
 
     // 状态栏
+    QStatusBar *sBar = statusBar();
+    QLabel *label = new QLabel(this);
+    label->setText("Normal text file");
+    sBar->addWidget(label);
+    // addWidget 从左往右添加
+    sBar->addWidget(new QLabel("2", this));
+    // addPermanentWidget 从右往左添加
+    sBar->addPermanentWidget(new QLabel("3", this));
+
     // 核心控件
+    QTextEdit *textEdit = new QTextEdit(this);
+    setCentralWidget(textEdit);
+
     // 浮动窗口
+    QDockWidget *dock = new QDockWidget(this);
+    addDockWidget(Qt::RightDockWidgetArea, dock);
+
+    // 给浮动窗口添加控件
+    QTextEdit *textEdit1 = new QTextEdit(this);
+    dock->setWidget(textEdit1);
 }
 
 MainWindow::~MainWindow()
